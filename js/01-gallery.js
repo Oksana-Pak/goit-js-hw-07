fe922e1fd5a9a33df0e006f808ffc5debe6b8dd2
Oksana-Pak <okspak7@gmail.com>
2022-10-07 22:02:46 +0300

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', createGallery);
+
+// ==============================================================
+
+galleryRef.addEventListener('click', onImageClick);
+
+let imgInstance;
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const urlLarge = getLargeUrl(e);
+
+  imgInstance = basicLightbox.create(`<img src=${urlLarge}>`);
+  imgInstance.show(onClickClose());
+
+  window.addEventListener('keydown', onEscKeyClose);
+}
+
+function getLargeUrl(e) {
+  return e.target.dataset.source;
+}
+
+function onEscKeyClose(e) {
+  if (e.code !== 'Escape') {
+    return;
+  }
+  imgInstance.close();
+}
+
+function onClickClose() {
+  window.removeEventListener('keydown', onEscKeyClose);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+ <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', createGallery);
